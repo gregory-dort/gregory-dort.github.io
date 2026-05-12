5496d34553241c064bf1efd8ed1f16a724a4b4de
Flebory <123499686+Flebory@users.noreply.github.com>
2026-05-12 15:31:56 -0400

    Updated Footer Component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
-        <div>
-
+        <div className='w-full space-y-2 p-4 mt-6 bg-white flex flex-col items-center justify-center'>
+            <p className='text-center text-gray-600 text-sm'>© 2025 Gregory Dort. All rights reserved.</p>
+            <p className='text-center text-gray-600 text-sm'>Built with React, Tailwind CSS, Framer Motion and ❤️</p>
         </div>
     );
 }
