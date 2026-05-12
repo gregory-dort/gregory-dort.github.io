0a82b25f24ed1efcadeae5d1f73f890970fcfffa
Flebory <123499686+Flebory@users.noreply.github.com>
2026-05-12 10:06:58 -0400

    Added Portrait Size Changes

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
                     variants={itemVariants}
                     src="/GregWC.jpg"
                     alt="Profile Picture"
-                    className="aspect-retro rounded-full object-cover mx-auto mb-4 border-2 border-gray-200 shadow-xl"
+                    className="aspect-video rounded-full object-cover mx-auto mb-6 border-2 border-gray-200 shadow-xl"
                 />
                 <motion.h1
                     variants={itemVariants} 
